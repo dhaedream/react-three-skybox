a3499c4f17d05e832a499acac6e0e0e407efed67
paintincode <93278553+paintincode@users.noreply.github.com>
2022-03-30 21:28:21 -0400

    unable to see <Skybox /> . no errors. just invisible.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ import "./styles.css";
 // big cube that surronds scene w teaxture
 function Skybox() {
   // three hook
-  const { scene } = useThree;
-  // cubeLoader accepts array to use images to wrap
+  const scene = useThree;
   const loader = new CubeTextureLoader();
+  // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
+  const texture = loader.load([
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+    "/6.jpg"
+  ]);
+  // Set the scene background property to the resulting texture.
+  scene.background = texture;
+  // null bc no geometry was used . So we return nothing
+  return null;
 }
 
 function Sphere() {
@@ -28,6 +40,7 @@ function Sphere() {
 export default function App() {
   return (
     <Canvas>
+      <Skybox />
       <Sphere />
     </Canvas>
   );
