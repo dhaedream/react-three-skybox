8e0657b3183bbaa0ca8c6dfab3f8fa9e06202d1d
paintincode <93278553+paintincode@users.noreply.github.com>
2022-03-30 20:59:50 -0400

    added sphere component to canvas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import "./styles.css";
 
+function Sphere() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
+
 export default function App() {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas>
+      <Sphere />
+    </Canvas>
+  );
 }
